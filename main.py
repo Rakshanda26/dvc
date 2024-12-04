73bc11384bbd037b9e4446a91d2a57b48f1def99
Rakshanda@26 <rakshandamm123@gmail.com>
2024-12-04 13:10:02 +0530

    updated

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ Data = [
     {"name" : "sunny", "age": 28, "city": "bhopal"},
     {"name" : "abhi", "age": 27, "city": "delhi"},
     {"name" : "ram", "age": 48, "city": "pune"},
-    {"name" : "shyam", "age": 38, "city": "banglore"}
+    {"name" : "shyam", "age": 38, "city": "banglore"},
+    {"name" : "mohan", "age": 32, "city": "banglore"}
 ]
 
 Data = pd.DataFrame(Data)
