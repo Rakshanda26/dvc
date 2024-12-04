c13e596f089fc8aa2915c461e13b0c9a10799ed9
Rakshanda@26 <rakshandamm123@gmail.com>
2024-12-04 18:51:09 +0530

    dvc.yaml added

---
+++ stage_01.py
@@ -0,0 +1,5 @@
+with open("artifact.txt", "w") as f:
+    f.write("my name is xyz and i am executing the stage one ")
+
+with open("artifact2.txt", "w") as f:
+    f.write("this is my second file ")
