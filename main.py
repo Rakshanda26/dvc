76cfcd91176b66c844e6fa569268ba07668aef68
Rakshanda@26 <rakshandamm123@gmail.com>
2024-12-04 12:56:35 +0530

    Initial commit of project code

---
+++ main.py
@@ -0,0 +1,13 @@
+import  pandas as pd
+
+Data = [
+
+    {"name" : "sunny", "age": 28, "city": "bhopal"},
+    {"name" : "abhi", "age": 27, "city": "delhi"},
+    {"name" : "ram", "age": 48, "city": "pune"},
+    {"name" : "shyam", "age": 38, "city": "banglore"}
+]
+
+Data = pd.DataFrame(Data)
+
+Data.to_csv("data/data.csv", index=False)
